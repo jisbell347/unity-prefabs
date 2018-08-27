0505c00ab76d8f2e5455a1cb68dacaafa31d10b7
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-08-27 11:01:46 -0600

    Added shootingDirection variable and replaced the Vector3.forward with it.

--- Bullet.cs
+++ Bullet.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
     public float shootingForce = 2000f;
+    public Vector3 shootingDirection;
 
 	// Use this for initialization
 	void Start () {
-	    GetComponent<Rigidbody>().AddForce(Vector3.forward * shootingForce);
+	    GetComponent<Rigidbody>().AddForce(shootingDirection * shootingForce);
 	}
 	
 	// Update is called once per frame
