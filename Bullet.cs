0eeb5e6f858fdd385585f3ee70ee9ef6fbc80896
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-08-27 10:52:51 -0600

    Defined the physics of the bullet object

---
+++ Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float shootingForce = 2000f;
+
+	// Use this for initialization
+	void Start () {
+	    GetComponent<Rigidbody>().AddForce(Vector3.forward * shootingForce);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
