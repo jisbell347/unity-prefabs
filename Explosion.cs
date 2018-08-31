e6d85b717157c28dd95caf38f34cefab9669c7a1
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-08-31 08:28:10 -0600

    Added code that controls the explosion game object

---
+++ Explosion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public GameObject[] particlePrefabs;
+    public int amountOfParticles = 3;
+    public float lifetime = 1f;
+
+	// Use this for initialization
+	void Start () {
+	    for (int i = 0; i < amountOfParticles; i++) {
+	        GameObject particlePrefab = Instantiate(particlePrefabs[Random.Range(0, particlePrefabs.Length)]);
+	        particlePrefab.transform.position = transform.position;
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    lifetime -= Time.deltaTime;
+	    if (lifetime <= 0) {
+	        Destroy(gameObject);
+	    }
+	}
+}
